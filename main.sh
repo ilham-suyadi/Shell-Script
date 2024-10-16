cb431e2dc4e2b9a67ba22c63f140e7bd4802aa7d
Ilham Suyadi <ilham.suyadi@solusi247.com>
2024-10-16 10:28:24 +0700

    add: rust

--- main.sh
+++ main.sh
@@ -2,13 +2,15 @@
 
 echo "1) k9s"
 echo "2) trivy"
-echo "3) quit"
+echo "3) rust"
+echo "4) quit"
 
 read -p "Please select the application you want to install : " choice
 
 case $choice in
     1) wget -O - https://raw.githubusercontent.com/ilham-suyadi/Shell-Script-Installer/main/k9s.sh | bash;;
     2) wget -O - https://raw.githubusercontent.com/ilham-suyadi/Shell-Script-Installer/main/trivy.sh | bash;;
+    4) curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh ;;
     3) echo "We're done";;
     *) echo "Oops, Invalid choice";;
 esac
