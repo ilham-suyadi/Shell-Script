8d8af34c920bd8ab01e967a44e35209f8d835cda
ilham suyadi <72326498+ilham-suyadi@users.noreply.github.com>
2025-02-06 06:38:07 +0000

    add Speed disk

--- main.sh
+++ main.sh
@@ -2,8 +2,9 @@
 
 echo "1) k9s"
 echo "2) trivy"
-echo "3) rust"
-echo "4) helm"
+echo "3) Rust"
+echo "4) Helm"
+echo "5) Speedtest Disk"
 echo "5) quit"
 
 read -p "Please select the application you want to install : " choice
@@ -12,7 +13,7 @@ case $choice in
     1) wget -O - https://raw.githubusercontent.com/ilham-suyadi/Shell-Script-Installer/main/k9s.sh | bash;;
     2) wget -O - https://raw.githubusercontent.com/ilham-suyadi/Shell-Script-Installer/main/trivy.sh | bash;;
     3) curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh ;;
-    5) wget -O - https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash ;;
-    6) echo "We're done";;
+    4) wget -O - https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash ;;
+    5) echo "We're done";;
     *) echo "Oops, Invalid choice";;
 esac
---
+++ speedtest_disk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dd if=/dev/zero of=/tmp/test1.img bs=1G count=1 oflag=dsync
